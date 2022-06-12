e62398964360f0f3281261d9176767ebda8c72b9
Ezequiel <martinez.ezequiel42@gmail.com>
2022-06-12 06:18:32 -0700

    Created entryManager to contain a static list of all available entry points for zombies. Also began pseudocoding out how entry logic will work in zombNav

---
+++ Assets/Scripts/GameManager Scripts/EntryManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static EntryManager instance = null;
+    public  List<GameObject> availableWindows = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+       {
+        instance = this;
+       }
+       else if (instance != this)
+       Destroy(gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ZombieScripts/ZombNav.cs
+++ Assets/Scripts/ZombieScripts/ZombNav.cs
@@ -8,11 +8,15 @@ public class ZombNav : MonoBehaviour
 
     private NavMeshAgent nm;
     public Transform navTarget;
+    private ZombAI zombAI; 
+
+    private bool isInside;
 
     // Start is called before the first frame update
     void Start()
     {
-        nm = gameObject.GetComponent<NavMeshAgent>();
+        
+        init();
         StartCoroutine(think());
     }
 
@@ -22,7 +26,48 @@ public class ZombNav : MonoBehaviour
         
     }
 
+
+    public void init()
+    {
+        nm = gameObject.GetComponent<NavMeshAgent>();
+        zombAI = this.GetComponent<ZombAI>();
+        isInside = false;
+    }
+
+
+
+    IEnumerator think()
+    {
+        while(true)
+        {
+        navTarget = calculateNavTarget().transform;
+        nm.SetDestination(navTarget.position);
+        yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+
+
     private GameObject calculateNavTarget()
+    {
+        if (isInside == true)
+        {
+            return calculateNearestPlayerNav();
+        } else 
+        {
+           return calculateNearestEntry();
+        }
+
+    }
+
+    private GameObject calculateNearestEntry()
+     {
+                // >find nearest window, if targetWindow.isValie==true >>> move to targetWindow.child("entryPoint")
+                // >if (isInside)
+     }
+
+
+    private GameObject calculateNearestPlayerNav()
     {
         GameObject[] players;
         players = GameObject.FindGameObjectsWithTag("Player");
@@ -40,13 +85,6 @@ public class ZombNav : MonoBehaviour
             }
         }
         return closest;
-    }
-
-    IEnumerator think()
-    {
-        while(true){navTarget = calculateNavTarget().transform;
-        nm.SetDestination(navTarget.position);
-        yield return new WaitForSeconds(0.5f);}
     }
 
 
